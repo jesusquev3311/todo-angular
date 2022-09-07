59e402d9ecfc79f496b21cb57afc2cd6315db502
Jesus Quevedo <jesus.quevedo@morningstar.com>
2022-09-06 21:44:49 -0400

    splitting item from list into a separate compponent

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Todo } from 'src/app/shared/todo/todo';
 
 @Component({
-  selector: 'app-todo-item',
+  selector: 'todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.scss']
 })
 export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo = {id: 0, title:'', completed: false, editing: false} ;
+  @Output() checkedItem = new EventEmitter();
+  @Output() deleteItem = new EventEmitter();
+  @Output() doubleClickItem = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  editTodo(todo: Todo): void{
+      this.doubleClickItem.emit(todo);
+  }
+
+  doneEdit(todo: Todo): void {
+      this.checkedItem.emit(todo);
+  }
+
+  removeTodo(todoId: number): void {
+      this.deleteItem.emit(todoId);
+  }
 }
